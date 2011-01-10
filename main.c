b43e779bc1fcbe02435f3b16684c145dc4527cee
the-light <ehsan.abdolahy@gmail.com>
2011-01-10 15:36:23 +0330

    change kilowatt to upercase  main.c

--- main.c
+++ main.c
@@ -1,49 +1,50 @@
- /*
- ============================================================================
- Name        : main.c
- Author      : ehsan abdolahi
- Version     :
- License     : gpl
- Description : ghabz bargh in C, Ansi-style
- email       : ehsan.abdolahy@gmail.com
- edited      : 10 January 2011, 13:08
- Created on 15 December 2010, 22:58
- ============================================================================
- */
+/*
+============================================================================
+Name        : main.c
+Author      : ehsan abdolahi
+Version     :
+License     : gpl
+Description : ghabz bargh in C, Ansi-style
+email       : ehsan.abdolahy@gmail.com
+edited      : 10 January 2011, 13:08
+Created on 15 December 2010, 22:58
+============================================================================
+*/
 
 #include <stdio.h>
-#define kilowatt 50
+#define KILOWATT 50
 
-int RoundNum(int sumkw) {
+int RoundNum(int sumkw)
+{
 
-unsigned int  bb = 0;    /*adade mohesebe shode baraye mahe badi*/
-int round = 0;  /* adade round shode baraye pardakhte moshtari */
+        unsigned int  bb = 0;    /*adade mohesebe shode baraye mahe badi*/
+        int round = 0;  /* adade round shode baraye pardakhte moshtari */
 
-    bb = sumkw % 100;  /* joda kardane adade khurde az adade asli */
-    printf("\nadade kasr shode baraye mahe baad =  %d rial", bb);
+        bb = sumkw % 100;  /* joda kardane adade khurde az adade asli */
+        printf("\nadade kasr shode baraye mahe baad =  %d rial", bb);
         round = sumkw - bb;  /* kasre khurde adad az adade asli va mohasebe adade round shode */
-    printf("\nmizan poole mohasebe shode baraye moshtari dar in mah = %d rial", round);
+        printf("\nmizan poole mohasebe shode baraye moshtari dar in mah = %d rial", round);
         printf("\n(^-^) (^_^) ");
 }
 
 int main()
 {
 
-int number,number2;    /* adad haye khande shode dar mahe 1 && 2*/
-int sumkw;           /* adade khande shode * 50 */
-int maliat;           /* 30% maliat */
+        int number,number2;    /* adad haye khande shode dar mahe 1 && 2*/
+        int sumkw;           /* adade khande shode * 50 */
+        int maliat;           /* 30% maliat */
 
-printf("addad khande shode az kontor ra vared konid(month1): ");
-    scanf("%d",&number);
-printf("adade khande shode az kontor ra vared konid(month2): ");
-    scanf("%d", &number2);
+        printf("addad khande shode az kontor ra vared konid(month1): ");
+        scanf("%d",&number);
+        printf("adade khande shode az kontor ra vared konid(month2): ");
+        scanf("%d", &number2);
         number -= number2; /*  kasr adad 1 az 2 va rikhtan dar adade aval */
-        sumkw = number * kilowatt; /*zarbe adad be dast amade dar 50 */
+        sumkw = number * KILOWATT; /*zarbe adad be dast amade dar 50 */
         sumkw += 12000;     /* adade be dast amade + 12000 maliat */
         maliat = sumkw / 30;        /* 30% maliat */
         sumkw += maliat;
 
-    RoundNum(sumkw);
+        RoundNum(sumkw);
 
-return 0;
-      }
+        return 0;
+}
